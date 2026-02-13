76ac6a8b477a50a5026c37f21317e3267d994601
Dave <davidbowles@pm.me>
2026-02-13 15:19:18 +0000

    Initial Commit

---
+++ Freezer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Freezer
+//
+//  Created by Dave Bowles on 13/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Freezer/FreezerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreezerApp.swift
+//  Freezer
+//
+//  Created by Dave Bowles on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FreezerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
